e5ed8157ad391ac90f5e7d05ff90eb5333df9620
EliteDevSolution <62186848+EliteDevSolution@users.noreply.github.com>
2021-12-07 12:07:06 +1000

    commi

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Fill your api key here
+  API_KEY: 'iKWLwriwyyUnpMJn2xt400RaY2NaKJu51hYTOkc6dN7GWDjHcFElWD7rqjNC2aDW',
+  // Fill your api secret here
+  API_SECRET: 'DPgiVMuvavPy7hSdKG6eg1Jf1beyahmoIntztV58TLPgmeTW2s5H99UMDmPY4UCC',
+  HTTP_INTERVAL: 500,
+}
