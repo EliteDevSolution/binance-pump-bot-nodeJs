56884b1844a4ccba5734e216340643c4e9fc1fb9
EliteDevSolution <62186848+EliteDevSolution@users.noreply.github.com>
2021-12-07 12:11:14 +1000

    sum

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Fill your api key here
-  API_KEY: 'iKWLwriwyyUnpMJn2xt400RaY2NaKJu51hYTOkc6dN7GWDjHcFElWD7rqjNC2aDW',
-  // Fill your api secret here
-  API_SECRET: 'DPgiVMuvavPy7hSdKG6eg1Jf1beyahmoIntztV58TLPgmeTW2s5H99UMDmPY4UCC',
-  HTTP_INTERVAL: 500,
-}
